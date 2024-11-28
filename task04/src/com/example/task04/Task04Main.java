2f5ae400b4ec5859fcc52cefd9bc1a435380c94d
Soniccooles <andrey.sonic@mail.ru>
2024-11-29 01:55:53 +0500

    task4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,36 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"))) {
 
+            Map<String, Long> wordFrequency = reader.lines()
+                    .flatMap(line -> Arrays.stream(line.split("\\P{L}+")))
+                    .map(String::toLowerCase)
+                    .filter(word -> !word.isEmpty())
+                    .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+
+            List<String> topWords = wordFrequency.entrySet().stream()
+                    .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
+                    .limit(10)
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+
+
+            String result = topWords.stream().collect(Collectors.joining("\n"));
+            System.out.print(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
